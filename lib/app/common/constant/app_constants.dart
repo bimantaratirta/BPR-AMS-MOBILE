c63b2d15d31dc613add13620fb1b96922bdd78b1
Muhammad Hendika Putra <muhamadhendika77@gmail.com>
2026-03-09 11:42:58 +0700

    chore: change url to production url

--- lib/app/common/constant/app_constants.dart
+++ lib/app/common/constant/app_constants.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   // API Constants
   // static const String baseApiUrl = "http://10.0.2.2:3001/api/v2";
-  static const String baseApiUrl = "https://chadwick-preludial-ayesha.ngrok-free.dev/api/v1";
-  static const String baseImageUrl = "https://minio.s3.nevmock.id/bpr-pms";
+  static const String baseApiUrl = "https://api.bpr.ashakita.net/api/v1";
+  static const String baseImageUrl = "https://is3.cloudhost.id/bpr-ams";
 
   // Endpoints
   static const String authEmployeePathApi = "/auth/employee";
